1dfd6989a8bb74d40f12e6b9a9416d7d5f04e4a0
kyraxwu <e0559825@u.nus.edu>
2023-11-10 18:35:44 +0800

    Week 12

--- McDonalds/app.R
+++ McDonalds/app.R
@@ -1,20 +1,57 @@
+# Load required libraries
 library(shiny)
+library(dplyr)
+library(ggplot2)
 
-# Define UI for application that draws a histogram
+# Define the UI for the Shiny app
 ui <- fluidPage(
+  titlePanel("Big Mac Price Explorer"),
   sidebarLayout(
-    sidebarPanel(sliderInput("samplesize","Sample Size:",min = 100,max = 10000,value = 1000)),
-    mainPanel(plotOutput("distPlot"))
+    sidebarPanel(
+      sliderInput("year", "Select a Year:", min = 2000, max = 2023, value = 2023),
+      br(),
+      downloadButton("downloadData", "Download Filtered Data")
+    ),
+    mainPanel(
+      plotOutput("pricePlot")
+    )
   )
 )
 
-# Define server logic required to draw a histogram
+# Define the server logic
 server <- function(input, output) {
-  output$distPlot <- renderPlot({
-    hist(rnorm(input$samplesize),col='darkorchid',xlab="Sample",main="Standard Normally Distributed Sample")},
-    height=300
+  # Load the CSV data
+  bigmac_data <- read.csv("/Users/kyrawu/kyraxwu.github.io/McDonalds/DataSet/BigmacPrice.csv", stringsAsFactors = FALSE)
+  
+  # Convert the 'date' column to a Date object
+  bigmac_data$date <- as.Date(bigmac_data$date)
+  
+  # Define a reactive expression for filtered data
+  filtered_data <- reactive({
+    bigmac_data %>%
+      filter(year(date) == input$year)
+  })
+  
+  # Create a plot of Big Mac prices
+  output$pricePlot <- renderPlot({
+    filtered_data() %>%
+      ggplot(aes(x = name, y = dollar_price, fill = currency_code)) +
+      geom_bar(stat = "identity") +
+      labs(title = "Big Mac Prices by Country", x = "Country", y = "Price in USD") +
+      theme_minimal() +
+      theme(axis.text.x = element_text(angle = 45, hjust = 1))
+  })
+  
+  # Create a download button for filtered data
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      paste("bigmac_prices_", input$year, ".csv", sep = "")
+    },
+    content = function(file) {
+      write.csv(filtered_data(), file)
+    }
   )
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+# Run the Shiny app
+shinyApp(ui, server)
