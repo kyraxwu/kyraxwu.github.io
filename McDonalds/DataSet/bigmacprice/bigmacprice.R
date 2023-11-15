90586eee012b0963b810f98ee297af6eeee2d5c4
kyraxwu <e0559825@u.nus.edu>
2023-11-15 16:19:37 +0800

    Updates

--- McDonalds/DataSet/bigmacprice/bigmacprice.R
+++ McDonalds/DataSet/bigmacprice/bigmacprice.R
@@ -2,20 +2,46 @@ library(shiny)
 library(readr)
 library(ggplot2)
 library(rnaturalearth)
-library(rnaturalearthdata)
 library(tidyverse)
+# ... [Other libraries and existing code] ...
 
 ui <- fluidPage(
-  # ... [Existing UI elements] ...
+  # Include custom CSS
+  tags$head(
+    tags$style(HTML("
+      body {background-color: #f7f7f7;} 
+      .well {background-color: #ffffff; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);} 
+      .shiny-input-container {padding: 10px;} 
+      .selectize-input {border-radius: 4px; border: 1px solid #ced4da; height: calc(2.25rem + 2px);} 
+      .selectize-dropdown {border-radius: 4px; border: 1px solid #ced4da;}
+      .btn {background-color: #007bff; color: white; border-radius: 4px;} 
+      .btn:hover {background-color: #0056b3;}
+      #bigMacTimeSeries {border-radius: 8px; overflow: hidden; background-color: #f9f9f9;}
+    "))
+  ),
   
-  # UI for time series chart
-  plotOutput("bigMacTimeSeries")
+  # App Content
+  fluidRow(
+    column(12, 
+           div(plotOutput("bigMacTimeSeries"), style = "border-radius: 8px; overflow: hidden;")
+    )
+  ),
+  fluidRow(
+    column(12, 
+           wellPanel(
+             selectInput("grouping", "Select Grouping:",
+                         choices = c("Continent" = "continent",
+                                     "Subregion" = "subregion",
+                                     "Economy" = "economy",
+                                     "Income Group" = "income_grp"))
+           ),
+           checkboxInput("showAnnotations", "Show Annotations", TRUE)
+           
+    )
+  )
 )
 
 server <- function(input, output, session) {
-  
-  # ... [Existing server logic] ...
-  
   # Read the Big Mac Index data
   bigMacData <- read_csv("BigmacPrice.csv") %>%
     mutate(name = case_when(
@@ -36,67 +62,54 @@ server <- function(input, output, session) {
     group_by(continent, date) %>%
     summarize(average_dollar_price = mean(dollar_price, na.rm = TRUE)) %>%
     ungroup()  
-  
-  ggplot(averagePriceByContinent, aes(x = date, y = average_dollar_price, group = continent, color = continent)) +
-    geom_line() +
-    theme_minimal() +
-    labs(title = "Big Mac Index Over Time", x = "Date", y = "Price (Local Currency)")
-  
   #avg by subregion
   averagePriceBysubregion <- bigMacData %>%
     group_by(subregion, date) %>%
     summarize(average_dollar_price = mean(dollar_price, na.rm = TRUE)) %>%
     ungroup()  
-  
-  ggplot(averagePriceBysubregion, aes(x = date, y = average_dollar_price, group = subregion, color = subregion)) +
-    geom_line() +
-    theme_minimal() +
-    labs(title = "Big Mac Index Over Time", x = "Date", y = "Price (Local Currency)")
-  
   #avg by economy
   averagePriceByeconomy <- bigMacData %>%
     group_by(economy, date) %>%
     summarize(average_dollar_price = mean(dollar_price, na.rm = TRUE)) %>%
     ungroup()  
-  
-  ggplot(averagePriceByeconomy, aes(x = date, y = average_dollar_price, group = economy, color = economy)) +
-    geom_line() +
-    theme_minimal() +
-    labs(title = "Big Mac Index Over Time", x = "Date", y = "Price (Local Currency)")
-  
   #avg by economy
   averagePriceByincome <- bigMacData %>%
     group_by(income_grp, date) %>%
     summarize(average_dollar_price = mean(dollar_price, na.rm = TRUE)) %>%
     ungroup()  
   
-  ggplot(averagePriceByincome, aes(x = date, y = average_dollar_price, group = income_grp, color = income_grp)) +
-    geom_line() +
-    theme_minimal() +
-    labs(title = "Big Mac Index Over Time", x = "Date", y = "Price (Local Currency)")
-
-  
+  selectedData <- reactive({
+    switch(input$grouping,
+           "continent" = averagePriceByContinent,
+           "subregion" = averagePriceBysubregion,
+           "economy" = averagePriceByeconomy,
+           "income_grp" = averagePriceByincome)
+  })
   # Render the time series chart
   output$bigMacTimeSeries <- renderPlot({
-    ggplot(averagePriceByincome, aes(x = date, y = average_dollar_price, group = income_grp, color = income_grp)) +
+    
+    # Start with the basic ggplot object
+    p <- ggplot(selectedData(), aes_string(x = "date", y = "average_dollar_price", group = input$grouping, color = input$grouping)) +
       geom_line() +
-      # Other vertical lines
-      geom_vline(xintercept = as.Date("2003-09-02"), linetype = "dashed", color = "green") +
-      geom_vline(xintercept = as.Date("2004-07-08"), linetype = "dashed", color = "red") +
-      # Translucent rectangles for events
-      geom_rect(aes(xmin = as.Date("2007-12-01"), xmax = as.Date("2009-06-30"), ymin = -Inf, ymax = Inf), fill = "red", alpha = 0.005) +
-      geom_rect(aes(xmin = as.Date("2020-03-01"), xmax = as.Date("2020-06-30"), ymin = -Inf, ymax = Inf), fill = "red", alpha = 0.005) +
-      # Captions
-      annotate("text", x = as.Date("2003-09-02"), y = Inf, label = "I'm Lovin' It launch", vjust = 1.5, color = "green") +
-      annotate("text", x = as.Date("2004-07-08"), y = Inf, label = "Super Size Me", vjust = 2, color = "red") +
-      annotate("text", x = as.Date("2007-12-01"), y = Inf, label = "Global Financial Crisis", vjust = 2.75, color = "red") +
-      annotate("text", x = as.Date("2020-03-01"), y = Inf, label = "COVID Lockdown", vjust = 3.75, color = "red") +
       theme_minimal() +
       labs(title = "Big Mac Index Over Time", x = "Date", y = "Price (Local Currency)")
+    
+    # Conditionally add annotations if the checkbox is checked
+    if (input$showAnnotations) {
+      p <- p + 
+        geom_vline(xintercept = as.Date("2003-09-02"), linetype = "dashed", color = "gold") +
+        geom_vline(xintercept = as.Date("2004-07-08"), linetype = "dashed", color = "red") +
+        geom_rect(aes(xmin = as.Date("2007-12-01"), xmax = as.Date("2009-06-30"), ymin = -Inf, ymax = Inf), fill = "red", alpha = 0.002) +
+        geom_rect(aes(xmin = as.Date("2020-03-01"), xmax = as.Date("2020-06-30"), ymin = -Inf, ymax = Inf), fill = "red", alpha = 0.002) +
+        annotate("text", x = as.Date("2003-09-02"), y = Inf, label = "I'm Lovin' It launch", vjust = 1.5, color = "gold", fontface = "bold") +
+        annotate("text", x = as.Date("2004-07-08"), y = Inf, label = "Super Size Me Documentary", vjust = 2, color = "red", fontface = "bold") +
+        annotate("text", x = as.Date("2007-12-01"), y = Inf, label = "Global Financial Crisis", vjust = 2.75, color = "red", fontface = "bold") +
+        annotate("text", x = as.Date("2020-03-01"), y = Inf, label = "COVID Lockdown", vjust = 3.75, color = "red", fontface = "bold")
+    }
+    
+    # Return the plot
+    p
   })
-  
-  # ... [Any additional server logic] ...
 }
 
-# ... [Rest of the existing code] ...
 shinyApp(ui, server)
--- McDonalds/DataSet/mcdonalds/mcdonalds.R
+++ McDonalds/DataSet/mcdonalds/mcdonalds.R
@@ -7,8 +7,36 @@ library(jsonlite)
 
 # Define UI
 ui <- fluidPage(
-  leafletOutput("map")
+  # Include custom CSS
+  tags$head(
+    tags$style(HTML("
+      body {background-color: #f7f7f7;} 
+      .well {background-color: #ffffff; border-radius: 8px; box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);} 
+      .shiny-input-container {padding: 10px;} 
+      #viewType label {font-weight: bold;} 
+      .leaflet-container {background: #f9f9f9; border-radius: 8px;} 
+      .btn {background-color: #007bff; color: white; border-radius: 4px;} 
+      .btn:hover {background-color: #0056b3;}
+    "))
+  ),
+  
+  # App Content
+  fluidRow(
+    column(12, 
+           div(leafletOutput("map"), style = "border-radius: 8px; overflow: hidden;")
+    )
+  ),
+  fluidRow(
+    column(12, 
+           wellPanel(
+             radioButtons("viewType", "Choose View:",
+                          choices = c("McDonald's Outlets per Million Residents" = "outlets_per_mil",
+                                      "GDP per Capita" = "gdp_per_capita"))
+           )
+    )
+  )
 )
+
 
 # Define server
 server <- function(input, output, session) {
@@ -17,45 +45,59 @@ server <- function(input, output, session) {
   df <- st_read("outlets.csv") 
   df <- df %>%
     mutate(
-      across(c(2, 3, 5, 6, 9, 10), as.numeric)
-    )  # Create a color palette function
-  palette <- colorQuantile("YlOrRd", df$outlets_per_mil)
+      across(c(2, 3, 5, 6, 9, 10), as.numeric), gdp_per_capita = gdp_md_est/pop_est*1000000
+    )  
   geometries <- st_as_sfc(df$geometry, crs = 4326) # Ensure the CRS is correct for your data
-  
   # Combine the geometries with the rest of the data frame
   df_sf <- st_sf(df, geometry = geometries)
   # Create Leaflet map with colored country borders
-  output$map <- renderLeaflet({
-    leaflet(df_sf) %>%
-      addProviderTiles(providers$CartoDB.Positron) %>%
-      addPolygons(
-        fillColor = ~palette(outlets_per_mil),
-        color = "#000000", # Border color
-        fillOpacity = 0.7,
-        weight = 1,
-        dashArray = "3",
-        smoothFactor = 0.5,
-        highlightOptions = highlightOptions(
-          weight = 2,
-          color = "#666",
-          dashArray = "",
+  
+  observe({
+    # Decide which data to display based on the input
+    if(input$viewType == "outlets_per_mil"){
+      colorData <- df$outlets_per_mil
+    } else {
+      colorData <- df$gdp_per_capita
+    }
+    breaks <- quantile(colorData, probs = seq(0, 1, by = 0.2), na.rm = TRUE)
+    palette <- colorBin("YlOrRd", colorData, bins = breaks, na.color = "#808080")
+    output$map <- renderLeaflet({
+      leaflet(df_sf) %>%
+        addProviderTiles(providers$CartoDB.Positron) %>%
+        addPolygons(
+          fillColor = ~palette(colorData), #fill according to selected data
+          color = "#000000", # Border color
           fillOpacity = 0.7,
-          bringToFront = TRUE
-        ),
-        label = ~paste(
-          "Country: ", name, "<br/>",
-          "Stores per Million: ", round(outlets_per_mil, 2), "<br/>",
-          "Number of Outlets: ", num_outlets, "<br/>",
-          "First Store Opened On: ", `First.Opening`,"<br/>",
-          "GDP (Billion USD): ", round(gdp_md_est/1000, 2)
-          
-        )%>%
-          lapply(htmltools::HTML)
-      ) %>%
-      setView(lat = 0, lng = 0, zoom = 2)
+          weight = 1,
+          dashArray = "3",
+          smoothFactor = 0.5,
+          highlightOptions = highlightOptions(
+            weight = 2,
+            color = "#666",
+            dashArray = "",
+            fillOpacity = 0.7,
+            bringToFront = TRUE
+          ),
+          label = ~paste(
+            "Country: ", name, "<br/>",
+            "Stores per Million: ", round(outlets_per_mil, 2), "<br/>",
+            "Number of Outlets: ", num_outlets, "<br/>",
+            "First Store Opened On: ", `First.Opening`,"<br/>",
+            "GDP (Billion USD): ", round(gdp_md_est/1000, 2)
+            
+          )%>%
+            lapply(htmltools::HTML)
+        ) %>%
+        addLegend("bottomright", 
+                  pal = palette, 
+                  values = ~colorData,
+                  title = ifelse(input$viewType == "outlets_per_mil", "McDonald's Outlets per Million Residents", "GDP per Capita"),
+                  labels = round(breaks),
+                  opacity = 1,) %>%
+        setView(lat = 0, lng = 0, zoom = 2)
+    })
   })
 }
-?addPolygons()
 
 # Run the application
 shinyApp(ui, server)
