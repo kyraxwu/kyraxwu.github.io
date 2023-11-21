4fc5128ac2661c967bad70a96b092b46d738d23f
kyraxwu <e0559825@u.nus.edu>
2023-11-21 22:14:20 +0800

    Commit

--- McDonalds/DataSet/McDNutrition/McDNutrition.R
+++ McDonalds/DataSet/McDNutrition/McDNutrition.R
@@ -9,45 +9,23 @@ library(factoextra)
 library(tidytext) 
 library(plotly)
 
-
-
 nutrition_data <- read_excel("pffy-data-mcdonalds-nutrition-facts.xlsx") %>% mutate(CATEGORY = tolower(CATEGORY))
-#nutrition_data <- nutrition_data %>%
-#mutate(ITEM = str_replace_all(ITEM, "\\s*\\((?!Small|Medium|Large).*\\)|\\s*\\d+\\s*(fl oz|oz|ml|g|lb|kg)\\b", ""))
-numdata = nutrition_data %>% select_if(is.numeric)
-corr_matrix <- cor(numdata, use = "complete.obs")  # 'use' parameter handles missing values
-
-# Step 4: Create the Correlation Plot
-corrplot(corr_matrix, method = "color")  # Using corrplot
-ggcorr(corr_matrix, hc.order = TRUE, type = "lower")  # Using GGally for a different style
-
-#nutrition_data_scaled <- scale(numdata)
-#lets get tags that we can use to filter search with:
-# Tokenize the menu items into words
 menu_items  = nutrition_data$ITEM
 words <- menu_items %>%
   as_tibble() %>%
   unnest_tokens(word, value) %>% filter(!str_detect(word, "\\d+")) 
 
-# Count word frequencies
 word_freq <- words %>%
   count(word, sort = TRUE)%>%filter(n>=8, nchar(word)>=3)
 
 tags <- word_freq$word
 
-# Function to add tags to CATEGORY
 add_tags <- function(item, category) {
-  # Find tags that are in the item
   matching_tags <- tags[sapply(tags, function(t) grepl(t, item, ignore.case = TRUE))]
-  
-  # Combine existing category with new tags, separated by commas
   new_category <- paste(c(category, matching_tags), collapse = ", ")
-  
-  # Return the updated category
   return(new_category)
 }
 
-# Apply the function to each row in nutrition_data
 nutrition_data <- nutrition_data %>%
   rowwise() %>%
   mutate(CATEGORY = add_tags(ITEM, CATEGORY))
@@ -66,12 +44,10 @@ ui <- fluidPage(
       radioButtons("tagLogic", "Tag Filter Logic:", choices = c("OR" = "or", "AND" = "and"))
     ),
     mainPanel(
-      plotlyOutput("nutrition"),  # Changed from plotOutput to plotlyOutput
-      plotOutput("corrPlot")
+      plotlyOutput("nutrition")
     )
   )
 )
-
 
 server <- function(input, output, session) {
   
@@ -79,12 +55,10 @@ server <- function(input, output, session) {
     data <- nutrition_data
     if (length(input$tags) > 0) {
       if (input$tagLogic == "and") {
-        # Apply AND logic
         for (tag in input$tags) {
           data <- data %>% filter(str_detect(CATEGORY, tag))
         }
       } else {
-        # Apply OR logic
         data <- data %>% filter(str_detect(CATEGORY, paste(input$tags, collapse = "|")))
       }
     }
@@ -92,27 +66,33 @@ server <- function(input, output, session) {
   })
   
   output$nutrition <- renderPlotly({
-    req(input$xaxis, input$yaxis)  # Ensure these inputs are selected
+    req(input$xaxis, input$yaxis)
     
-    # Create ggplot object
-    p <- ggplot(filtered_data(), aes_string(x = input$xaxis, y = input$yaxis, text = "ITEM")) +
-      geom_point(alpha = 0.7) +
+    p <- ggplot(filtered_data(), aes_string(x = input$xaxis, y = input$yaxis)) +
+      geom_point(alpha = 0.7, aes(text = paste("<b style='font-size:20px;'>", ITEM, "</b><br>",
+                                               "X-Axis:", input$xaxis, ": ", get(input$xaxis), "<br>",
+                                               "Y-Axis:",input$yaxis, ": ", get(input$yaxis), "<br>",
+                                               "<br>",
+                                               "Calories: ", CAL, "<br>",
+                                               "Total Fat (g): ", FAT, "<br>",
+                                               "Trans Fat (g): ", TFAT, "<br>",
+                                               "Saturated Fat (g): ", SFAT, "<br>",
+                                               "Cholesterol (mg): ", CHOL, "<br>",
+                                               "Sodium (mg): ", SALT, "<br>",
+                                               "Carbohydrates (g): ", CARB, "<br>",
+                                               "Dietary Fiber (g): ", FBR, "<br>",
+                                               "Sugars (g): ", SGR, "<br>",
+                                               "Protein (g): ", PRO))) +
       labs(title = "Nutrition Data Scatter Plot", x = input$xaxis, y = input$yaxis) +
       theme_minimal()
     
-    # If color is selected, add it to the plot
     if (input$color != "None") {
-      p <- p + aes_string(color = input$color)
+      p <- p + aes_string(color = input$color) +
+        scale_color_gradient(low = "#FFC30B", high = "red")
     }
     
-    # Convert to Plotly and add tooltip
-    ggplotly(p, tooltip = "text")  # Tooltip shows the value of "ITEM"
-  })
-  
-  output$corrPlot <- renderPlot({
-    corrplot(corr_matrix, method = "color")
+    ggplotly(p, tooltip = "text")
   })
 }
-
 
 shinyApp(ui, server)
--- McDonalds/DataSet/bigmacprice/bigmacprice.R
+++ McDonalds/DataSet/bigmacprice/bigmacprice.R
@@ -3,10 +3,8 @@ library(readr)
 library(ggplot2)
 library(rnaturalearth)
 library(tidyverse)
-# ... [Other libraries and existing code] ...
 
 ui <- fluidPage(
-  # Include custom CSS
   tags$head(
     tags$style(HTML("
       body {background-color: #f7f7f7;} 
@@ -16,11 +14,10 @@ ui <- fluidPage(
       .selectize-dropdown {border-radius: 4px; border: 1px solid #ced4da;}
       .btn {background-color: #007bff; color: white; border-radius: 4px;} 
       .btn:hover {background-color: #0056b3;}
-      #bigMacTimeSeries {border-radius: 8px; overflow: hidden; background-color: #f9f9f9;}
+      #bigMacTimeSeries {border-radius: 8px; overflow: hidden; background-color: #CBC3E3;}
     "))
   ),
   
-  # App Content
   fluidRow(
     column(12, 
            div(plotOutput("bigMacTimeSeries"), style = "border-radius: 8px; overflow: hidden;")
@@ -33,16 +30,15 @@ ui <- fluidPage(
                          choices = c("Continent" = "continent",
                                      "Subregion" = "subregion",
                                      "Economy" = "economy",
-                                     "Income Group" = "income_grp"))
-           ),
-           checkboxInput("showAnnotations", "Show Annotations", TRUE)
-           
+                                     "Income Group" = "income_grp")),
+             checkboxInput("showAnnotations", "Show Annotations", TRUE)
+           )
     )
   )
 )
 
+
 server <- function(input, output, session) {
-  # Read the Big Mac Index data
   bigMacData <- read_csv("BigmacPrice.csv") %>%
     mutate(name = case_when(
       name == "Hong Kong" ~ "Hong Kong S.A.R.",
@@ -85,29 +81,27 @@ server <- function(input, output, session) {
            "economy" = averagePriceByeconomy,
            "income_grp" = averagePriceByincome)
   })
-  # Render the time series chart
+  
   output$bigMacTimeSeries <- renderPlot({
     
-    # Start with the basic ggplot object
     p <- ggplot(selectedData(), aes_string(x = "date", y = "average_dollar_price", group = input$grouping, color = input$grouping)) +
       geom_line() +
       theme_minimal() +
-      labs(title = "Big Mac Index Over Time", x = "Date", y = "Price (Local Currency)")
+      labs(title = "Big Mac Index Over Time", x = "Date", y = "Price (Local Currency)") + 
+      scale_color_brewer(palette = "Set2") +
+      theme(panel.background = element_rect(fill = "#F5F5F5")) 
     
-    # Conditionally add annotations if the checkbox is checked
     if (input$showAnnotations) {
       p <- p + 
-        geom_vline(xintercept = as.Date("2003-09-02"), linetype = "dashed", color = "gold") +
+        geom_vline(xintercept = as.Date("2003-09-02"), linetype = "dashed", color = "#ffc10B") +
         geom_vline(xintercept = as.Date("2004-07-08"), linetype = "dashed", color = "red") +
         geom_rect(aes(xmin = as.Date("2007-12-01"), xmax = as.Date("2009-06-30"), ymin = -Inf, ymax = Inf), fill = "red", alpha = 0.002) +
         geom_rect(aes(xmin = as.Date("2020-03-01"), xmax = as.Date("2020-06-30"), ymin = -Inf, ymax = Inf), fill = "red", alpha = 0.002) +
-        annotate("text", x = as.Date("2003-09-02"), y = Inf, label = "I'm Lovin' It launch", vjust = 1.5, color = "gold", fontface = "bold") +
-        annotate("text", x = as.Date("2004-07-08"), y = Inf, label = "Super Size Me Documentary", vjust = 2, color = "red", fontface = "bold") +
-        annotate("text", x = as.Date("2007-12-01"), y = Inf, label = "Global Financial Crisis", vjust = 2.75, color = "red", fontface = "bold") +
-        annotate("text", x = as.Date("2020-03-01"), y = Inf, label = "COVID Lockdown", vjust = 3.75, color = "red", fontface = "bold")
+        annotate("text", x = as.Date("2003-09-02"), y = Inf, label = "I'm Lovin' It launch", vjust = 2, color = "#ffc10b", fontface = "bold") +
+        annotate("text", x = as.Date("2004-07-08"), y = Inf, label = "Super Size Me Documentary", vjust = 4, color = "red", fontface = "bold") +
+        annotate("text", x = as.Date("2007-12-01"), y = Inf, label = "Global Financial Crisis", vjust = 2, color = "red", fontface = "bold") +
+        annotate("text", x = as.Date("2020-03-01"), y = Inf, label = "COVID Lockdown", vjust = 2, color = "red", fontface = "bold")
     }
-    
-    # Return the plot
     p
   })
 }
